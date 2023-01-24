64afb777de806717f43100fe834b8278d7937c40
Shiv3006 <116787360+Shiv3006@users.noreply.github.com>
2023-01-24 18:20:57 +0530

    Add files via upload

---
+++ ReverseAlbhaOrderUsingComparator.java
@@ -0,0 +1,15 @@
+package com.bnt.collection;
+
+import java.util.TreeSet;
+
+public class ReverseAlbhaOrderUsingComparator {
+public static void main(String[] args) {
+	TreeSet<String> t=new TreeSet<String>();
+	t.add("Shiv");
+	t.add("Ruhi");
+	t.add("Swara");
+	t.add("Ananya");
+	t.add("Usha");
+	System.out.println(t);
+}
+}
